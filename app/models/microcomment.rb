a2b0681a00105342db66d93077334340e4bc33b2
gibil <gibil@oblique.org>
2014-09-25 15:35:14 -0500

    ..

--- app/models/microcomment.rb
+++ app/models/microcomment.rb
@@ -1,9 +1,10 @@
 
+# jr@oblique:  25/9/14
 
 
 class Microcomment < ActiveRecord::Base
 
 	# Validation block 
-  	validates :owner,  presence: true, length: { maximum: 50 } 
-  	validates :content,  presence: true, length: { maximum: 500 } 
+  	validates :owner,  presence: true    #, length: { maximum: 50 } 
+  	validates :content,  presence: true  #, length: { maximum: 500 } 
 end
--- spec/models/microcomment_spec.rb
+++ spec/models/microcomment_spec.rb
@@ -1,5 +1,31 @@
+
+# jr@oblique:  25/9/14
+
+
 require 'spec_helper'
 
 describe Microcomment do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  #let(:user) { FactoryGirl.create(:user) }
+
+  before do
+    # This code is not idiomatically correct.
+    #@micropost = Micropost.new(content: "Lorem ipsum", user_id: user.id)
+    @microcomment = Microcomment.new(owner: "Javier", content: "This is just a microcomment...")
+  end
+
+  subject { @microcomment }
+
+  it { should respond_to(:owner) }
+  it { should respond_to(:content) }
+
+
+  it { should be_valid }
+
+
+  describe "when owner is not present" do
+    before { @microcomment.owner = nil }
+    it { should_not be_valid }
+  end
+
 end
